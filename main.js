f5504a1426f2c6318353177452ee641bc05cdf42
D-Stukes <dstukes888@gmail.com>
2022-09-14 12:29:20 -0400

    working on making retrieved art clickable to redirect to more info, testing modal, added css grid

--- main.js
+++ main.js
@@ -17,11 +17,12 @@ formSelectArt.addEventListener(('submit'), (event) => {
         .then((response) => response.json())
         .then((data) => {
             console.log(data)
-            console.log(data.objectURL)
+           // console.log(data.objectURL)
     
         const selectedArtUrl = data.objectURL    
-        const displayArtDiv = document.querySelector('displayArt')
-        const artImage = document.createElement('img')
+        const displayArt = document.querySelector('.displayArt')
+        let artImage = document.createElement('img')
+        let artLink = document.querySelector('.artLink')
         // const iframe = document.createElement('iframe')
         // iframe.src = selectedArtUrl
         // console.log(selectedArtUrl)
@@ -30,17 +31,74 @@ formSelectArt.addEventListener(('submit'), (event) => {
 
         // document.body.append(iframe)
         // if(iframe){iframe.remove}
-        artImage.style.width = '500px'
-        artImage.style.height = '500px'
-        artImage.src = selectedArtUrl
+  
         // displayArtDiv.append(iframe)
         // document.body.append(artImage)
-        artImage.setAttribute('src', selectedArt)
-        artImage.setAttribute('height', 300)
-        artImage.setAttribute('width', 300)
-        artImage.setAttribute('alt', "art image")
+        artImage.setAttribute('src', data.primaryImage)
+        artImage.setAttribute('height', 530)
+        artImage.setAttribute('width', 500)
+        artImage.setAttribute('alt', "art piece")
+        artImage.style.border = ('2px solid brown')
         displayArt.append(artImage)
+        // artLink.setAttribute('href', data.objectURL )
+        
+        // anchor.appendChild(artImage)
+        artImage.addEventListener(('click'), (event) => {
+           event.preventDefault()
+           displayArt.style.display = "none"
+           const displayMore = document.querySelector('.displayMore')
+           displayMore.style.width = 500
+           displayMore.style.height = 530
+           displayMore.appendChild(artLink)
+        })
 
+
+///testing a modal
+const open_artModal_button = document.querySelectorAll('.open_artModal')
+const close_artModal_button = document.querySelector('.close_artModal')
+const artModalOverlay = document.querySelector('.artModal-overlay')
+
+// Attach event listeners
+open_artModal_button.addEventListener('click', open_artModal)
+artModal_overlay.addEventListener('click', close_artModal)
+document.addEventListener('keydown', close_artModal)
+
+// Utility functions to open/close the modal
+function open_artModal() {
+  artModal_overlay.classList.remove('hidden')
+}
+
+function close_artModal(event) {
+  if (
+    event.target.classList.contains('close-modal') ||
+    event.target.id === 'artModal-overlay' ||
+    (event.type === 'keydown' && event.key === 'Escape')
+  ) {
+    artModal_overlay.classList.add('hidden');
+  }
+}
+
+
+
+
+        
+        // function showArtInfo(){
+        //    // var image = document.getElementById('image');
+        //     let source = artPic.src;
+        //     window.open(source);
+        // }
+        // artImage.addEventListener(('click'), (event) => {
+        //     event.preventDefault()
+        //     artImage.setAttribute('src', `${selectedArtUrl}`)
+    //    anchor.setAttribute('href', data.objectURL)
+    //    anchor.setAttribute('target', '_blank')
+        // artImage.onclick = () => {
+        //     // window.location = `${selectedArtUrl}`;
+        //     anchor.setAttribute('href', `${selectedArtUrl}`);
+        //     // a.target = "_blank"
+        // };
+        // artImage.onclick = showArtInfo()
+        // })
         formSelectArt.reset()
 
         })
